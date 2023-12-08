09e5e6ac5d3c793e22bd2137177e8779d4473872
Nguyen Thu Thao <ntthuthao1121@gmail.com>
2023-12-08 22:04:25 +0700

    feat: [B0] Init project

---
+++ src/model/dao/ConnectionToDB.java
@@ -0,0 +1,22 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionToDB {
+    private static Connection cnn;
+    public static Connection ConnectToMySQL() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3333/BKLibrary";
+            cnn = DriverManager.getConnection(url, "root", "");
+            return cnn;
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Lỗi kết nối với MySQL Server: " + e.getMessage());
+        }
+        return null;
+    }
+}
